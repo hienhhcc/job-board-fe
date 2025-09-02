f17e2919f076caebb85631b298a5d707491debf9
Hien Vu <vuvinhhien.work@gmail.com>
2025-09-02 21:05:15 +0700

    Add sidebar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+import AppSidebarClient from "@/components/_AppSidebarClient";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
 export default function HomePage() {
-  return <h1>Hi</h1>;
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl">WorkHive</span>
+          </SidebarHeader>
+          <SidebarContent>
+            <SidebarGroup />
+            <SidebarGroup />
+          </SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>abcdef</SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main></main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
---
+++ src/components/_AppSidebarClient.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { ReactNode } from "react";
+
+export default function AppSidebarClient({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <div className="flex flex-col w-full">
+        <div className="p-2 border-b flex items-center gap-1">
+          <SidebarTrigger />
+          <span className="text-xl">WorkHive</span>
+        </div>
+        <div className="flex flex-1">{children}</div>
+      </div>
+    );
+  }
+
+  return children;
+}

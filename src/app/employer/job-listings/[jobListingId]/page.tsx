90c9ae5774c0b006de22541cddc62f24a75569dc
hienhhcc <thachdau16t@gmail.com>
2025-09-08 16:47:09 +0700

    Initial view job listing

--- src/app/employer/job-listings/[jobListingId]/page.tsx
+++ src/app/employer/job-listings/[jobListingId]/page.tsx
@@ -1,8 +1,16 @@
+import { MarkdownPartial } from "@/components/markdown/MarkdownPartial";
+import MarkdownRenderer from "@/components/markdown/MarkdownRenderer";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { env } from "@/data/env/client";
 import { getJobListingOrganizationTag } from "@/features/job-listings/cache";
+import JobListingBadges from "@/features/job-listings/components/JobListingBadges";
+import { formatJobListingStatus } from "@/features/job-listings/lib/formatters";
 import { getCurrentOrganization } from "@/services/clerk/lib/getCurrentAuth";
 import { FullJobListing } from "@/types";
 import { auth } from "@clerk/nextjs/server";
+import { EditIcon } from "lucide-react";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
@@ -35,8 +43,31 @@ async function SuspendedPage({ params }: Props) {
           <h1 className="text-2xl font-bold tracking-tight">
             {jobListing.title}
           </h1>
+          <div className="flex flex-wrap gap-2 mt-2">
+            <Badge>{formatJobListingStatus(jobListing.status)}</Badge>
+            <JobListingBadges jobListing={jobListing} />
+          </div>
+        </div>
+        <div className="flex items-center gap-2 empty:-mt:4">
+          <Button asChild variant="outline">
+            <Link href={`/employer/job-listings/${jobListing.id}/edit`}>
+              <EditIcon className="size-4" />
+              Edit
+            </Link>
+          </Button>
         </div>
       </div>
+
+      <MarkdownPartial
+        dialogMarkdown={<MarkdownRenderer source={jobListing.description} />}
+        mainMarkdown={
+          <MarkdownRenderer
+            source={jobListing.description}
+            className="prose-sm"
+          />
+        }
+        dialogTitle="Description"
+      />
     </div>
   );
 }
---
+++ src/components/markdown/MarkdownRenderer.tsx
@@ -0,0 +1,27 @@
+import { markdownClassNames } from "@/components/markdown/_MarkdownEditor";
+import { cn } from "@/lib/utils";
+import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
+import remarkGfm from "remark-gfm";
+
+export default function MarkdownRenderer({
+  className,
+  options,
+  ...props
+}: MDXRemoteProps & { className?: string }) {
+  return (
+    <div className={cn(markdownClassNames, className)}>
+      <MDXRemote
+        {...props}
+        options={{
+          mdxOptions: {
+            remarkPlugins: [
+              remarkGfm,
+              ...(options?.mdxOptions?.remarkPlugins ?? []),
+            ],
+            ...options?.mdxOptions,
+          },
+        }}
+      />
+    </div>
+  );
+}
